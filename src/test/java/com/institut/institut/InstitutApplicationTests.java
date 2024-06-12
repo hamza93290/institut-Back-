6b392ab092675b2197179a89ca8f1fd81233cf45
hamza <66466122+hamza93290@users.noreply.github.com>
2024-06-13 00:26:23 +0200

    first

---
+++ src/test/java/com/institut/institut/InstitutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.institut.institut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
